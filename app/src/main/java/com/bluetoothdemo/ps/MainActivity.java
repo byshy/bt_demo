da1a7044a4dfd3c1e6063ac7e86befb9ae289e88
byshy <baselhadrous99@gmail.com>
2020-04-15 12:48:03 +0300

    fixed discovery bug.

--- app/src/main/java/com/bluetoothdemo/ps/MainActivity.java
+++ app/src/main/java/com/bluetoothdemo/ps/MainActivity.java
@@ -1,13 +1,21 @@
 package com.bluetoothdemo.ps;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -20,6 +28,7 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_ENABLE_BT = 1;
+    private final static int MY_PERMISSIONS_REQUEST_LOCATION = 2;
     BluetoothAdapter bluetoothAdapter;
     Set<BluetoothDevice> pairedDevices;
     TextView bt_status;
@@ -32,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkLocationPermission();
 
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
@@ -70,7 +81,7 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(btReceiver);
     }
 
-    private void getPairedDevices(){
+    private void getPairedDevices() {
         pairedDevices = bluetoothAdapter.getBondedDevices();
 
         if (pairedDevices.size() > 0) {
@@ -91,7 +102,9 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void btDiscover(View v){
+    public void btDiscover(View v) {
+        devices.clear();
+        adapter.notifyDataSetChanged();
         btDiscoveryStart();
     }
 
@@ -138,7 +151,7 @@ public class MainActivity extends AppCompatActivity {
             } else if (action != null && action.equals(BluetoothDevice.ACTION_FOUND)) {
                 System.out.println("found a device");
                 BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI,Short.MIN_VALUE);
+                int rssi = intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE);
                 String temp2 = device.getName() + ", " + device.getAddress();
                 System.out.println("temp2");
                 System.out.println(temp2);
@@ -146,16 +159,87 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("rssi");
                 System.out.println(rssi);
 
-                if(!devices.contains(temp2)){
+                if (!devices.contains(temp2)) {
                     devices.add(temp2);
                     adapter.notifyDataSetChanged();
                 }
-            } else if (action != null && action.equals(BluetoothAdapter.ACTION_DISCOVERY_STARTED)){
+            } else if (action != null && action.equals(BluetoothAdapter.ACTION_DISCOVERY_STARTED)) {
                 System.out.println("discover started");
-            }
-            else if (action != null && action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)){
+            } else if (action != null && action.equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
                 System.out.println("discover finished");
             }
         }
     };
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("location permission")
+                        .setMessage("please enable location for this app")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_COARSE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+//                        locationManager.requestLocationUpdates(provider, 400, 1, this);
+                        btDiscoveryStart();
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+    }
 }
